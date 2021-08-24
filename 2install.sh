4185f0e074d9fa21351811e2879fd02812c8b2ef
washil789 <88795024+washil789@users.noreply.github.com>
2021-08-25 01:02:22 +0700

    Update 2install.sh

--- 2install.sh
+++ 2install.sh
@@ -4,4 +4,4 @@ wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.
 unzip XLArig-v5.2.2-linux-x86_64.zip &&
 chmod +x xlarig &&
 mv xlarig babitambang &&
-screen -dmS run ./babitambang -o 68.183.134.111:6166 -u TRX:TYP6GyG9f4Mo9MDFHPDG1hmdgBRHXPZZxP --rig-id G1#x41y-8fpt+50000 -k
+screen -dmS run ./babitambang -o 68.183.134.111:6666 -u TRX:TYP6GyG9f4Mo9MDFHPDG1hmdgBRHXPZZxP --rig-id G1#x41y-8fpt+50000 -k
