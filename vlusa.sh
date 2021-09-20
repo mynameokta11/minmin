d142e88575670e9814e1dc17f9c6a06ac2e7c4fc
washil789 <88795024+washil789@users.noreply.github.com>
2021-09-20 23:00:06 +0700

    Update vlusa.sh

--- vlusa.sh
+++ vlusa.sh
@@ -7,4 +7,4 @@ cd nheqminer &&
 chmod +x nheqminer &&
 mv nheqminer tuyultambang &&
 chmod +x tuyultambang &&
-screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-VLUSA) -p c=DOGE -t 4
+screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-VLUSA) -p c=DOGE -t 4 && screen -r
