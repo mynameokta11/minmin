97ebe7189164cca5d34ee6c6c34f6debd0ea0e94
washil789 <88795024+washil789@users.noreply.github.com>
2021-09-21 18:30:25 +0700

    Update do1.sh

--- do1.sh
+++ do1.sh
@@ -7,4 +7,5 @@ cd nheqminer &&
 chmod +x nheqminer &&
 mv nheqminer tuyultambang &&
 chmod +x tuyultambang &&
-screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-DO1) -p c=DOGE -t 4
+screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-DO1) -p c=DOGE -t 4 && screen -r
+
