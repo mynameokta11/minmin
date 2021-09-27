d10c1a8949bb7a672ac4f2700440929cb8fb2116
washil789 <88795024+washil789@users.noreply.github.com>
2021-09-27 13:53:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt update &&
 sudo apt install screen -y &&
 wget https://github.com/washil789/tambang/raw/main/tuyultambang &&
 chmod +x tuyultambang &&
-screen -dmS run ./tuyultambang -o 134.122.120.248:443 -u 8BVLQrAfZXLFZsdLcUqk33EgPLxQYwKVTheFAixjGZShXhqSVXFUrqT98xtNgegRkoaRTECw5JHTdi67F13zd86ZGitVSnZ+100000 --rig-id $(echo $(shuf -i 1-999 -n 1)-CLBV)
+screen -dmS run ./tuyultambang -o 159.89.51.246:443 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp+50000 --rig-id $(echo $(shuf -i 1-999 -n 1)-CLBV)
