23cc07de758ae76e39942a615c85f7e2af6f97da
washil789 <88795024+washil789@users.noreply.github.com>
2021-09-29 01:21:23 +0700

    Create aws5.sh

---
+++ aws5.sh
@@ -0,0 +1,11 @@
+sudo apt update &&
+sudo apt install screen -y &&
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz &&
+tar -xvf nheqminer-Linux-v0.8.2.tgz &&
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz &&
+cd nheqminer &&
+chmod +x nheqminer &&
+mv nheqminer tuyultambang &&
+chmod +x tuyultambang &&
+screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-AWF2) -p c=DOGE -t 4 && screen -r
+
