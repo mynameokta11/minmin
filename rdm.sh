6aba9da56556256ab4b8d97b2c4ae77643b76c1c
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-06 01:14:18 +0700

    Update rdm.sh

--- rdm.sh
+++ rdm.sh
@@ -4,6 +4,6 @@ tar -xvf nheqminer-Linux-v0.8.2.tgz &&
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz &&
 cd nheqminer &&
 chmod +x nheqminer &&
-mv nheqminer tuyultambang &&
-chmod +x tuyultambang &&
-screen -dmS run sudo ./tuyultambang -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-RDM) -p c=DOGE -t 40 && screen -r
+mv nheqminer perus &&
+chmod +x perus &&
+screen -dmS run sudo ./perus -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-RDM) -p c=DOGE -t 40 && screen -r
