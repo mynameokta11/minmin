d7a4d87f46be9987b7acafe94ea112f6687be920
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-06 00:24:03 +0700

    Update lnd1.sh

--- lnd1.sh
+++ lnd1.sh
@@ -1,4 +1,3 @@
-sudo apt update &&
 sudo apt install screen -y &&
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz &&
 tar -xvf nheqminer-Linux-v0.8.2.tgz &&
