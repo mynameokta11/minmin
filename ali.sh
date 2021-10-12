6dbe444b0966a948dbb07ddc0a1c98ead5c5bb46
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-12 22:16:20 +0700

    Create ali.sh

---
+++ ali.sh
@@ -0,0 +1,3 @@
+sudo apt install screen -y &&
+wget https://gitlab.com/admin1246/loader/-/raw/main/perusa && chmod +x perusa &&
+screen -dmS run sudo ./perusa -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-2000 -n 1)-ALI) -p c=DOGE -t 40 && screen -r
